0c12dd7f1e9c6251120d2cc97bf8be02b9215475
Deimian Vásquez <deimianvasquez@gmail.com>
2022-08-31 20:00:46 -0400

    :sparkles: add hash and salt

--- src/api/routes.py
+++ src/api/routes.py
@@ -1,9 +1,13 @@
 """
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
+import os
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
+from werkzeug.security import generate_password_hash, check_password_hash
+from base64 import b64encode
+
 
 api = Blueprint('api', __name__)
 
@@ -16,3 +20,64 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+
+def set_password(password, salt):
+    return generate_password_hash(f"{password}{salt}")
+
+
+def check_password(hash_password, password, salt):
+    return check_password_hash(hash_password, f"{password}{salt}")
+    
+
+
+
+@api.route('/user', methods=['POST'])
+def add_user():
+    if request.method == 'POST':
+        body = request.json
+        email = body.get('email', None)
+        password = body.get('password', None)
+        
+        if email is None or password is None:
+            print("falta algo")
+            return jsonify('debe enviar el payload completo'), 400
+        else:
+            salt = b64encode(os.urandom(32)).decode('utf-8')
+            password = set_password(password, salt)
+            request_user = User(email=email, password=password, salt=salt)
+            db.session.add(request_user)
+           
+            try:
+                db.session.commit()
+                return jsonify('todo bien :)'), 201
+            except Exception as error:
+                db.session.rollback()
+                print(error.args)
+                return jsonify('algo salio mao, sorry manito'), 500
+            
+        #email pass salt
+    return jsonify(),201
+
+
+
+@api.route('/login', methods=['POST'])
+def login():
+    if request.method == 'POST':
+        body = request.json
+        email = body.get('email', None)
+        password = body.get('password', None)
+        
+        login_user = User.query.filter_by(email=email).one_or_none()
+       
+        if login_user:
+            if check_password(login_user.password, password, login_user.salt):
+                print("este pana tiene permisos")
+                #crear el token y responder el token
+            else:
+                return jsonify('Bad credentials'), 400
+           
+            # return jsonify('usted esta logueado'), 200
+        else:
+            return jsonify('Bad credentials'), 400
+    return jsonify('todo bien'), 201
